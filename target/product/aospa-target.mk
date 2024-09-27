06500079bdc90a1ffb6e84ac1d96aaf51b3f068e
musfiqurrahmanfahim <rmusfiqur971@gmail.com>
2024-09-27 21:24:04 +0600

    Update aospa-target.mk

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -97,6 +97,8 @@ PRODUCT_PACKAGES += \
     vendor.aospa.power-service
 
 # Google - GMS, Pixel, and Mainline Modules
+# $(call inherit-product, vendor/google/gms/config.mk)
+# $(call inherit-product, vendor/google/pixel/config.mk)
 ifneq ($(TARGET_EXCLUDE_GMODULES), true)
 $(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules.mk)
 endif
